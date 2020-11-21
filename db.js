9cafb48af977b4259950c14c9fde0d6f84372280
Tung Lam Nguyen Ba <tunglam.ngxba@gmail.com>
2020-11-22 01:36:03 +0700

    init with clientData Schema and userService

---
+++ db.js
@@ -0,0 +1,14 @@
+// require("dotenv").config();
+var mongoose = require("mongoose");
+const db = {
+    url : `mongodb+srv://admin:admin@cluster0-qvxth.mongodb.net/Project1?retryWrites=true&w=majority`,
+    
+    url : `mongodb+srv://admin:admin@cluster0.hsod6.mongodb.net/The2000thViettel?retryWrites=true&w=majority`,
+    option : {
+        useNewUrlParser : true // de sp mongo cho su dung db cua no
+    }
+}
+
+mongoose.connect(db.url, db.option);
+
+module.exports = mongoose;
---
+++ index.js
@@ -0,0 +1,66 @@
+// require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+const port = 5000
+const app = express();
+
+var userService = require("./domain/services/userService");
+var dataService = require("./domain/services/dataService");
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    hello: "world",
+  });
+});
+
+app.post("/api/user/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await userService.signUp(name, email, password);
+    res.json(newUser);
+  } catch (err) {
+    res.status(400);
+    res.json({
+      err: err.message,
+    });
+  }
+});
+
+app.post("/api/user/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const fecthedUser = await userService.signIn(email, password);
+    res.json(fecthedUser);
+  } catch (err) {
+    res.status(400);
+    res.json({
+      err: err.message,
+    });
+  }
+});
+
+app.post("/api/user/update", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const updatedUsser = await userService.editUser(name, email);
+    res.json(updatedUsser);
+  } catch (err) {
+    res.status(400);
+    res.json({
+      err: err.message,
+    });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
